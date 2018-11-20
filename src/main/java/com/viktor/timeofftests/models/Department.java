c7ec3eba4d46cd55e33c866f2f0d37cfc807d408
Viktor <v.melnychuk12@gmail.com>
2018-11-20 21:12:59 +0200

    impl

--- src/main/java/com/viktor/timeofftests/models/Department.java
+++ src/main/java/com/viktor/timeofftests/models/Department.java
@@ -1,4 +1,58 @@
 package com.viktor.timeofftests.models;
 
+import com.viktor.timeofftests.services.CompanyService;
+
 public class Department {
+    private int id;
+    private String name;
+    private int allowance;
+    private boolean includePublicHolidays;
+    private boolean isAccuredAllowance;
+    private int companyId;
+    private int bossId;
+
+    private Department(){}
+
+    public static class Builder {
+        private int id;
+        private String name;
+        private int allowance;
+        private boolean includePublicHolidays;
+        private boolean isAccuredAllowance;
+        private int companyId;
+        private int bossId;
+
+
+        public Builder withName(String name){
+            this.name = name;
+            return  this;
+        }
+
+        public  Builder withAllowance (int allowance){
+            this.allowance = allowance;
+            return  this;
+        }
+
+        public Builder includePublicHolidays(boolean includePublicHolidays){
+            this.includePublicHolidays = includePublicHolidays;
+            return this;
+        }
+
+        public Builder inCompany(String name){
+            this.companyId = CompanyService.getInstance().getOrCreateCompanyWithName(name).getId();
+            return this;
+        }
+
+        public Builder withAdminUserId(int id){
+            this.bossId = id;
+            return this;
+        }
+
+        public Department buildAndStore(){
+            // TODO: Finish this!!!!!
+            Department department = new Department();
+            return department;
+        }
+
+    }
 }
