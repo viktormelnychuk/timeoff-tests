b4967339fd969cb8d8515102f76c1d8c33f9939a
Viktor <v.melnychuk12@gmail.com>
2019-01-11 19:13:08 +0200

    Shit does not work!

---
+++ src/main/java/com/viktor/timeofftests/common/LogConfigurer.java
@@ -0,0 +1,38 @@
+package com.viktor.timeofftests.common;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.*;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+
+import java.util.Properties;
+
+public class LogConfigurer {
+
+    public static void configureLogger(String dirPath){
+        Properties props = System.getProperties();
+        props.setProperty("log4j2.debug","TRUE");
+        ConfigurationBuilder<BuiltConfiguration> builder = ConfigurationBuilderFactory.newConfigurationBuilder();
+        AppenderComponentBuilder console = builder.newAppender("stdout", "Console");
+
+        AppenderComponentBuilder file = builder.newAppender("logfile","File");
+        String fileName = dirPath + "qa_log.log";
+        file.addAttribute("fileName", fileName);
+
+        LayoutComponentBuilder layout = builder.newLayout("PatternLayout");
+        layout.addAttribute("pattern","[%level]::[%c]::[%d{DEFAULT_MICROS}]::%m%n");
+        console.add(layout);
+        file.add(layout);
+
+        LoggerComponentBuilder fileLogger = builder.newLogger("com.viktor.timeofftests", Level.DEBUG);
+        fileLogger.add(builder.newAppenderRef("logfile"));
+
+        RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.DEBUG);
+        rootLogger.add(builder.newAppenderRef("stdout"));
+        builder.add(rootLogger);
+        builder.add(console);
+        builder.add(file);
+        builder.add(fileLogger);
+        Configurator.initialize(builder.build());
+    }
+}
--- src/test/java/com/viktor/timeofftests/RunCucumberTest.java
+++ src/test/java/com/viktor/timeofftests/RunCucumberTest.java
@@ -2,6 +2,7 @@ package com.viktor.timeofftests;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
@@ -9,4 +10,9 @@ import org.junit.runner.RunWith;
         plugin = {"io.qameta.allure.cucumber3jvm.AllureCucumber3Jvm"}
 )
 public class RunCucumberTest {
+//
+//    @BeforeClass
+//    public static void beforeClass(){
+//        System.out.println("ASDASDASDASDASDASDASDASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+//    }
 }
